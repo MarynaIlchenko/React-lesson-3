072494dffc5f38e45cfa8599dca8e968f406e863
MarynaIlchenko <debovamail@gmail.com>
2022-05-06 21:04:36 +0300

    update

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './Counter.css';
 
 class Counter extends React.Component {
+  handleIncrement = () => {
+    console.log('Кликнули в увеличить');
+    console.log(this);
+  };
+
+  handleDecrement = () => {
+    console.log('Кликнули в уменьшить');
+    console.log(this);
+  };
+
   render() {
     return (
       <div className="Counter">
         <span className="Counter__value">0</span>
 
         <div className="Counter__controls">
-          <button type="button">Увеличить на 1</button>
-          <button type="button">Уменьшить на 1</button>
+          <button type="button" onClick={this.handleIncrement}>
+            Увеличить на 1
+          </button>
+          <button type="button" onClick={this.handleDecrement}>
+            Уменьшить на 1
+          </button>
         </div>
       </div>
     );
