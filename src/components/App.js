f2864b9387c2a2480e3513d2f371afd9710a51c5
MarynaIlchenko <debovamail@gmail.com>
2022-05-06 20:17:06 +0300

    update

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Counter from '../components/Counter/Counter';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#608D88' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F5185' },
+];
+
+const App = () => {
+  <>
+    <h1>Состояние компонента</h1>
+    <Counter />
+  </>;
+};
+
+export default App;
